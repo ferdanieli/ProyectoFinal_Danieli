a1900e745401f429e214c3e7949486372a10b6e5
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-12-10 14:00:03 -0300

    actualizacion

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import show_html, CrearCarrera, DetalleCarrera, CarreraList
+from blog.views import show_html, CrearCarrera, DetalleCarrera, CarreraList, ActualizacionCarrera, EliminarCarrera
 
 urlpatterns = [
     path('crear/', CrearCarrera.as_view()),
     path('carreras/', CarreraList.as_view(), name= "CarreraList"),
     path('carrera/<int:pk>', DetalleCarrera.as_view(), name="CarreraDetail"),
+    path('editar/<int:pk>', ActualizacionCarrera.as_view(), name="CarreraUpdate"),
+    path('eliminar/<int:pk>', EliminarCarrera.as_view(), name="CarreraDelete"),
     path('show/', show_html),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from blog.models import Carrera
 
 
@@ -17,6 +17,18 @@ class CarreraList(ListView):
 class DetalleCarrera(DetailView):
     model = Carrera
     template_name = "blog/detalle_carrera.html"
+
+class ActualizacionCarrera(UpdateView):
+    model = Carrera
+    success_url = "/blog/carreras/"
+    template_name = "blog/crear_carrera.html"
+    fields = ["descripcion", "imagen"]
+
+
+class EliminarCarrera(DeleteView):
+    model = Carrera
+    success_url = "/blog/carreras/"
+    template_name = "blog/eliminar_carrera.html"
 
 
 def show_html(request):
