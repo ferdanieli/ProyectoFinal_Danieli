cefb8053ffe9baaec5fa34b7a39ef022a47d5dd7
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-12-12 20:37:10 -0300

    últimos cambios

--- mensajes/admin.py
+++ mensajes/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from mensajes.models import Comentario
 
 admin.site.register(Comentario)
+
--- mensajes/models.py
+++ mensajes/models.py
@@ -3,10 +3,14 @@ from django.db import models
 import datetime
 from blog.models import Carrera
 
-
 class Comentario(models.Model):
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.CharField(max_length=255)
     fecha_creacion = models.DateTimeField(default=datetime.datetime.now())
+
+
+
+
+
 
